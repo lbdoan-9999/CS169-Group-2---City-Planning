0e51a7a17b841ad709c9fb965e150293e316c2fe
SorchaJ <sorchie@comcast.net>
2022-12-04 21:39:21 -0800

    done

--- main.py
+++ main.py
@@ -17,10 +17,10 @@ adjList = ga.generate_population(512, 8)
 # get smallest m and n possible to fit k unique locations in a grid
 m, n = ga.generate_dimensions(len(adjList.array))
 
-k_max = 500
-population = ga.rand_population(adjList, m, n, 100)
+k_max = 5*len(adjList.array)
+population = ga.rand_population(adjList, m, n, int(k_max*.2))
 
-S = ga.TournamentSelection(50)
+S = ga.TournamentSelection(int(k_max*.1))
 
 x, time = ga.genetic_algorithm(ga.f, population, k_max, S, adjList)
 
