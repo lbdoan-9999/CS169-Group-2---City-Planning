4d5ad7426c1d12d0c42f4d154addd5e6291cc07c
SorchaJ <sorchie@comcast.net>
2022-12-04 21:26:00 -0800

    Fixed genetic algo for main

--- main.py
+++ main.py
@@ -9,7 +9,23 @@ TRAVERSAL_SIZE  = 8
 # ==== IMPORTS =============================================================== #
 
 print("Running genetic algorithm...")
-import genetic_algo_method as gam
+import genetic_algo_method as ga
+
+
+adjList = ga.generate_population(512, 8)
+
+# get smallest m and n possible to fit k unique locations in a grid
+m, n = ga.generate_dimensions(len(adjList.array))
+
+k_max = 500
+population = ga.rand_population(adjList, m, n, 100)
+
+S = ga.TournamentSelection(50)
+
+x, time = ga.genetic_algorithm(ga.f, population, k_max, S, adjList)
+
+print("(" + str(ga.f(x, adjList)) + ", " + str(k_max) + ", " + str(time) + ")")
+
 
 print("Running graph method...")
 import graph_method        as gm
